09ef5256489ea2e9ba61dd5e672815c9bf6364d0
SergioArroyoDuque <sarroyoduque@gmail.com>
2020-07-16 19:09:16 +0200

    Revert "Añadido Hazelcast"
    This reverts commit 3642670181ff9afda37504448c85cc4d96077645.

--- muuzik/src/main/java/es/codeurjc/dad/Application.java
+++ muuzik/src/main/java/es/codeurjc/dad/Application.java
@@ -1,36 +1,12 @@
 package es.codeurjc.dad;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.JoinConfig;
-
-@EnableHazelcastHttpSession
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	
-	@Bean
-	public Config config() {
-		Config config = new Config();
-		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
-		joinConfig.getMulticastConfig().setEnabled(false);
-
-		List<String> serversList = new ArrayList<String>();
-		serversList.add("web");
-		serversList.add("web2");
-
-		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(serversList);
-		return config;
-	}
 }
-
