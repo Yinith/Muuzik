6d5e469828a05d5aba76a5bf4cf894dbcadd1c73
Yinith <48563463+Yinith@users.noreply.github.com>
2020-07-19 12:23:06 +0200

    Caché Anuncios añadida
    Funcionamiento: La caché se rellena al hacer una query buscando anuncios en la BBDD, por ejemplo al visitar tablón. Se vacía cuando se modifica el repositorio de anuncios, por ejemplo al borrar un anuncio o meter uno nuevo. En las siguientes búsquedas, se usa la información de la caché.
    
    - Subidos nuevos ejecutables .jar con la caché incorporada.
    
    @sergioarroyoduque
    
    Co-Authored-By: sergioarroyoduque <60467596+sergioarroyoduque@users.noreply.github.com>

--- muuzik/src/main/java/es/codeurjc/dad/Application.java
+++ muuzik/src/main/java/es/codeurjc/dad/Application.java
@@ -3,18 +3,26 @@ package es.codeurjc.dad;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.JoinConfig;
 
+@EnableCaching
 @SpringBootApplication
 @EnableHazelcastHttpSession
 public class Application {
 
+	private static final Log LOG = LogFactory.getLog(Application.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
@@ -31,5 +39,11 @@ public class Application {
 
 		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(serversList);
 		return config;
+	}
+
+	@Bean
+	public CacheManager cacheManager() { //Creación del objeto
+		LOG.info("Activating cache...");
+		return new ConcurrentMapCacheManager("anuncios"); //Hay una caché para anuncios
 	}
 }
